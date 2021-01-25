242c728afde1c61606be4a2f04952629caba9065
PROG WEB - MANHA <PROG WEB - MANHA@MININT-QVG78E3>
2021-01-25 12:57:00 -0200

    Exclusão de conteúdos que não serão utilizados. Alteração do nome do usuário e foto.

--- index.php
+++ index.php
@@ -7,34 +7,42 @@ if ($pg) {
     switch ($_GET['pg']) {
         case 'login':
             include_once 'painel/paginas/acesso/login.php';
-
             break;
-            include_once 'painel/paginas/dashboard.php';
 
-            break;
-        
         case 'inicial':
             include_once 'painel/site/inicio.php';
 
             break;
-            include_once 'painel/paginas/dashboard.php';
 
-            break;
         
         case 'dashboard':
             if (verificarlogin()){
                 
+            include_once 'painel/paginas/includes/header.php';
+            include_once 'painel/paginas/includes/menus.php';
             include_once 'painel/paginas/dashboard.php';
+            include_once 'painel/paginas/includes/footer.php';
+            }
+              else {
+                echo 'Login ou Senha inválido';
             }
             break;
         
         default:
+            include_once 'painel/paginas/includes/header.php';
+            include_once 'painel/paginas/includes/menus.php';
             include_once 'painel/paginas/dashboard.php';
+            include_once 'painel/paginas/includes/footer.php';
+          
+            
             break;
     }
 } else {
 //não existes    
+            include_once 'painel/paginas/includes/header.php';
+            include_once 'painel/paginas/includes/menus.php';
             include_once 'painel/paginas/dashboard.php';
+            include_once 'painel/paginas/includes/footer.php';
 }
 
         
