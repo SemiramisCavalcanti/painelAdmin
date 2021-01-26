62fd5ab0dff16830f55a9410c86c970d4f1bfdbc
PROG WEB - MANHA <PROG WEB - MANHA@MININT-QVG78E3>
2021-01-26 12:59:52 -0200

    Ajustes nos arquivos de dataTables.

--- painel/paginas/produtos.php
+++ painel/paginas/produtos.php
@@ -1,22 +1,147 @@
 
-  <!-- Content Wrapper. Contains page content -->
-  <div class="content-wrapper">
+<!-- Content Wrapper. Contains page content -->
+<div class="content-wrapper">
     <!-- Content Header (Page header) -->
     <div class="content-header">
-      <div class="container-fluid">
-        <div class="row mb-2">
-          <div class="col-sm-6">
-            <h1 class="m-0">Produtos </h1>
-          </div><!-- /.col -->
-          <div class="col-sm-6">
-            <ol class="breadcrumb float-sm-right">
-              <li class="breadcrumb-item"><a href="#">Home</a></li>
-              <li class="breadcrumb-item active">Dashboard v1</li>
-            </ol>
-          </div><!-- /.col -->
-        </div><!-- /.row -->
-      </div><!-- /.container-fluid -->
+        <div class="container-fluid">
+            <div class="row mb-2">
+                <div class="col-sm-6">
+                    <h1 class="m-0">Produtos </h1>
+                </div><!-- /.col -->
+                <div class="col-sm-6">
+                    <ol class="breadcrumb float-sm-right">
+                        <li class="breadcrumb-item"><a href="#">Home</a></li>
+                        <li class="breadcrumb-item active">Dashboard v1</li>
+                    </ol>
+                </div><!-- /.col -->
+            </div><!-- /.row -->
+        </div><!-- /.container-fluid -->
     </div>
     <!-- /.content-header -->
-  </div>
- 
+
+    <!-- Main content -->
+    <section class="content">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-12">
+                    <div class="card">
+                        <div class="card-header">
+                            <h3 class="card-title">DataTable com recursos mínimos e estilos de foco</h3>
+                        </div>
+                        <!-- /.card-header -->
+
+                        <div class="card">
+                            <div class="card-header">
+                                <h3 class="card-title">DataTable com recursos padrão</h3>
+                            </div>
+                            <!-- /.card-header -->
+                            <div class="card-body">
+                                <table id="example1" class="table table-bordered table-striped table-hover">
+                                    <thead>
+                                        <tr>
+                                            <th>Ordenar lista</th>
+                                            <th>Navegador</th>
+                                            <th>Plataformas</th>
+                                            <th>Versão</th>
+                                            <th>CSS grade</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <tr>
+                                            <td>Trident</td>
+                                            <td>Internet
+                                                Explorer 4.0
+                                            </td>
+                                            <td>Win 95+</td>
+                                            <td> 4</td>
+                                            <td>X</td>
+
+                                        <tr>
+                                            <td>Tasman</td>
+                                            <td>Internet Explorer 5.2</td>
+                                            <td>Mac OS 8-X</td>
+                                            <td>1</td>
+                                            <td>C</td>
+                                        </tr>
+                                        <tr>
+                                            <td>Misc</td>
+                                            <td>NetFront 3.1</td>
+                                            <td>Embedded devices</td>
+                                            <td>-</td>
+                                            <td>C</td>
+                                        </tr>
+                                        <tr>
+                                            <td>Misc</td>
+                                            <td>NetFront 3.4</td>
+                                            <td>Embedded devices</td>
+                                            <td>-</td>
+                                            <td>A</td>
+                                        </tr>
+                                        <tr>
+                                            <td>Misc</td>
+                                            <td>Dillo 0.8</td>
+                                            <td>Embedded devices</td>
+                                            <td>-</td>
+                                            <td>X</td>
+                                        </tr>
+                                        <tr>
+                                            <td>Misc</td>
+                                            <td>Links</td>
+                                            <td>Text only</td>
+                                            <td>-</td>
+                                            <td>X</td>
+                                        </tr>
+                                        <tr>
+                                            <td>Misc</td>
+                                            <td>Lynx</td>
+                                            <td>Text only</td>
+                                            <td>-</td>
+                                            <td>X</td>
+                                        </tr>
+                                        <tr>
+                                            <td>Misc</td>
+                                            <td>IE Mobile</td>
+                                            <td>Windows Mobile 6</td>
+                                            <td>-</td>
+                                            <td>C</td>
+                                        </tr>
+                                        <tr>
+                                            <td>Misc</td>
+                                            <td>PSP browser</td>
+                                            <td>PSP</td>
+                                            <td>-</td>
+                                            <td>C</td>
+                                        </tr>
+                                        <tr>
+                                            <td>Other browsers</td>
+                                            <td>All others</td>
+                                            <td>-</td>
+                                            <td>-</td>
+                                            <td>U</td>
+                                        </tr>
+                                    </tbody>
+                                    <tfoot>
+                                        <tr>
+                                            <th>Ordenar lista</th>
+                                            <th>Navegador</th>
+                                            <th>Plataformas</th>
+                                            <th>Versão</th>
+                                            <th>CSS grade</th>
+                                        </tr>
+                                    </tfoot>
+                                </table>
+                            </div>
+                            <!-- /.card-body -->
+                        </div>
+                    </div>
+                    <!-- /.card -->
+
+                </div>
+                <!-- /.col -->
+            </div>
+            <!-- /.row -->
+        </div>
+        <!-- /.container-fluid -->
+    </section>
+    <!-- /.content -->
+</div>
