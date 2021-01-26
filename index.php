74d94c14d94a3a42ab511eefdfe09bf0d34c9fc4
PROG WEB - MANHA <PROG WEB - MANHA@MININT-QVG78E3>
2021-01-26 10:26:08 -0200

    Inclusão pagina Produtos e inclusão do daschboard pagina no index

--- index.php
+++ index.php
@@ -1,9 +1,14 @@
 <?php
+
 include_once 'painel/helper/funcoes.php';
 
-$pg = isset( $_GET['pg']);
+$pg = isset($_GET['pg']);
 
 if ($pg) {
+
+    include_once 'painel/paginas/includes/header.php';
+    include_once 'painel/paginas/includes/menus.php';
+
     switch ($_GET['pg']) {
         case 'login':
             include_once 'painel/paginas/acesso/login.php';
@@ -11,38 +16,39 @@ if ($pg) {
 
         case 'inicial':
             include_once 'painel/site/inicio.php';
+           break;
 
+        case 'produtos':
+            include_once 'painel/paginas/produtos.php';
             break;
-
         
         case 'dashboard':
-            if (verificarlogin()){
-                
-            include_once 'painel/paginas/includes/header.php';
-            include_once 'painel/paginas/includes/menus.php';
             include_once 'painel/paginas/dashboard.php';
-            include_once 'painel/paginas/includes/footer.php';
-            }
-              else {
+            break;
+
+
+        case 'dashboard':
+            if (verificarlogin()) {
+
+               
+                include_once 'painel/paginas/dashboard.php';
+               
+            } else {
                 echo 'Login ou Senha inválido';
             }
             break;
-        
+
         default:
-            include_once 'painel/paginas/includes/header.php';
-            include_once 'painel/paginas/includes/menus.php';
             include_once 'painel/paginas/dashboard.php';
-            include_once 'painel/paginas/includes/footer.php';
-          
-            
             break;
     }
+    include_once 'painel/paginas/includes/footer.php';
 } else {
 //não existes    
-            include_once 'painel/paginas/includes/header.php';
-            include_once 'painel/paginas/includes/menus.php';
-            include_once 'painel/paginas/dashboard.php';
-            include_once 'painel/paginas/includes/footer.php';
+    include_once 'painel/paginas/includes/header.php';
+    include_once 'painel/paginas/includes/menus.php';
+    include_once 'painel/paginas/dashboard.php';
+    include_once 'painel/paginas/includes/footer.php';
 }
 
         
--- painel/paginas/produtos.php
+++ painel/paginas/produtos.php
@@ -6,7 +6,7 @@
       <div class="container-fluid">
         <div class="row mb-2">
           <div class="col-sm-6">
-            <h1 class="m-0">Dashboard - </h1> <h5>pagina inicial do painel administrativo</h5>
+            <h1 class="m-0">Produtos </h1>
           </div><!-- /.col -->
           <div class="col-sm-6">
             <ol class="breadcrumb float-sm-right">
@@ -20,4 +20,3 @@
     <!-- /.content-header -->
   </div>
  
-   
